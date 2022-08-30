c22d2f0774d348f52bdff9fbe790ec7f78aa7e30
diegoferna <diego.ferna@hotmail.com>
2022-08-30 15:04:47 -0300

    finalizando buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,30 @@
 import styles from './Cardapio.module.scss'
 import {ReactComponent as Logo} from 'assets/logo.svg'
+import Buscador from './Buscador'
+import { useState } from 'react'
+import Filtros from './Filtros'
 
 export default function Cardapio(){
+    const [busca, setBusca] = useState('')
+    const [filtro, setFiltro] = useState<number | null>(null)
     return(
         <main>
             <nav className={styles.menu}>
                 <Logo/>
             </nav>
+            
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A Casa do Código e da Massa!
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardapio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+                <div className={styles.cardaio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                </div>
+            </section>
         </main>
     )
 }
